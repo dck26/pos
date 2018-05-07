689b571808126a6e74f9a636ccb1f79d360e9948
dck26 <daniel.cuadra@gmail.com>
2018-05-06 21:56:51 -0500

    v1.6

--- vistas/modulos/usuarios.php
+++ vistas/modulos/usuarios.php
@@ -84,7 +84,7 @@
                       <td>
 
                         <div class="btn-group">
-                          <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                          <button class="btn btn-warning btnEditarUsuario" idUsuario="'.$value["id"].'" data-toggle="modal" data-target="#modalEditarUsuario"><i class="fa fa-pencil"></i></button>
                           <button class="btn btn-danger"><i class="fa fa-times"></i></button>
                         </div>
 
@@ -199,5 +199,99 @@
 
 </div>
   
-  <!--====  End of VENTANA MODAL USUARIOS  ====-->
+  <!--====  End of VENTANA MODAL AGREGAR USUARIOS  ====-->
+
+
+   <!--============================================
+  =            VENTANA MODAL EDITAR USUARIOS            =
+  =============================================-->
+  
+  <div id="modalEditarUsuario" class="modal fade" role="dialog">
+  
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+
+      <form role="form" method="post" enctype="multipart/form-data">
+
+        <div class="modal-header" style="background:#3c8dbc; color:white;">
+
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+          <h4 class="modal-title">Editar usuario</h4>
+
+        </div>
+
+        <div class="modal-body">
+          <div class="box-body">
+            <!-- Entrada nombre -->
+            <div class="form-group">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                <input type="text" class="form-control input-lg" name="editarNombre" id="editarNombre" value="" required>
+              </div>
+            </div>
+              <!-- Entrada usuario -->
+              <div class="form-group">
+                <div class="input-group">
+                  <span class="input-group-addon"><i class="fa fa-key"></i></span>
+                  <input type="text" class="form-control input-lg" name="editarUsuario" id="editarUsuario" value="" required>
+                </div>
+            </div>
+              <!-- Entrada password -->
+            <div class="form-group">
+                <div class="input-group">
+                  <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                  <input type="password" class="form-control input-lg" name="editarPassword" placeholder="Ingrese una nueva contraseña" required>
+                </div>
+            </div>
+              <!-- Entrada para seleccionar perfil -->
+            <div class="form-group">
+                <div class="input-group">
+                  <span class="input-group-addon"><i class="fa fa-users"></i></span>
+                  <select class="form-control input-lg" name="editarPerfil">
+
+                    <option value="" id="editarPerfil"></option>
+                    <option value="Administrador">Administrador</option>
+                    <option value="Especial">Especial</option>
+                    <option value="Vendedor">Vendedor</option>
+
+                  </select>
+                </div>
+            </div>
+            <!-- Entrada password -->
+            <div class="form-group">
+               <div class="panel">
+
+                <input type="file" class="nuevaFoto" name="editarFoto">
+                <p class="help-block">Peso máximo de la foto 4 MB</p>
+                <img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail previsualizar" width="100px">
+
+               </div>
+            </div>
+            </div>
+
+          </div>
+
+        <div class="modal-footer">
+
+          <button type="button" class="btn btn-primary pull-left" data-dismiss="modal">Salir</button>
+          <button type="submit" class="btn btn-primary">Guardar</button>
+
+        </div>
+        <?php
+
+          /*$crearUsuario = new ControladorUsuarios();
+          $crearUsuario -> ctrCrearUsuario();*/
+
+        ?>
+    </form>
+
+    </div>
+
+  </div>
+
+</div>
+  
+  <!--====  End of VENTANA MODAL EDITAR USUARIOS  ====-->
   
